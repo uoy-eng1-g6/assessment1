5732e7b243871df7cf19ab357391717bcb7b5983
tandemdude <olijdthompson@gmail.com>
2024-03-17 16:22:27 +0000

    Remove unnecessary code when creating static physics objects

---
+++ core/src/io/github/uoyeng1g6/models/PhysicsPolygon.java
@@ -0,0 +1,39 @@
+package io.github.uoyeng1g6.models;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhysicsPolygon {
+    private String name;
+    private BodyDef.BodyType type;
+    private Vector2 position;
+    private Vector2[] vertices;
+
+    public PhysicsPolygon() {}
+
+    public PhysicsPolygon(String name, BodyDef.BodyType type, Vector2 position, Vector2... vertices) {
+        this.name = name;
+        this.type = type;
+        this.position = position;
+        this.vertices = vertices;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BodyDef.BodyType getType() {
+        return type;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public Vector2[] getVertices() {
+        return vertices;
+    }
+}
--- core/src/io/github/uoyeng1g6/screens/Playing.java
+++ core/src/io/github/uoyeng1g6/screens/Playing.java
@@ -3,6 +3,7 @@ package io.github.uoyeng1g6.screens;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.PooledEngine;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -14,6 +15,7 @@ import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -29,6 +31,7 @@ import io.github.uoyeng1g6.constants.ActivityType;
 import io.github.uoyeng1g6.constants.MoveDirection;
 import io.github.uoyeng1g6.constants.PlayerConstants;
 import io.github.uoyeng1g6.models.GameState;
+import io.github.uoyeng1g6.models.PhysicsPolygon;
 import io.github.uoyeng1g6.systems.AnimationSystem;
 import io.github.uoyeng1g6.systems.DebugSystem;
 import io.github.uoyeng1g6.systems.MapRenderingSystem;
@@ -62,7 +65,6 @@ public class Playing implements Screen {
         this.gameState = new GameState();
         this.world = new World(new Vector2(0, 0), true);
 
-        initWalls();
         initTerrain();
 
         engine.addEntity(initPlayerEntity(engine));
@@ -82,112 +84,22 @@ public class Playing implements Screen {
         }
     }
 
-    void initWalls() {
-        var left = new BodyDef();
-        left.type = BodyDef.BodyType.StaticBody;
-        left.position.set(new Vector2(0, (float) WORLD_HEIGHT / 2));
-        var leftBody = world.createBody(left);
-        var leftBox = new PolygonShape();
-        leftBox.setAsBox(0.025f, (float) WORLD_HEIGHT / 2);
-        leftBody.createFixture(leftBox, 0f);
-        leftBox.dispose();
-
-        var right = new BodyDef();
-        right.type = BodyDef.BodyType.StaticBody;
-        right.position.set(new Vector2(WORLD_WIDTH, (float) WORLD_HEIGHT / 2));
-        var rightBody = world.createBody(right);
-        var rightBox = new PolygonShape();
-        rightBox.setAsBox(0.025f, (float) WORLD_HEIGHT / 2);
-        rightBody.createFixture(rightBox, 0f);
-        rightBox.dispose();
-
-        var top = new BodyDef();
-        top.type = BodyDef.BodyType.StaticBody;
-        top.position.set(new Vector2((float) WORLD_WIDTH / 2, WORLD_HEIGHT));
-        var topBody = world.createBody(top);
-        var topBox = new PolygonShape();
-        topBox.setAsBox((float) WORLD_WIDTH / 2, 0.025f);
-        topBody.createFixture(topBox, 0f);
-        topBox.dispose();
-
-        var bottom = new BodyDef();
-        bottom.type = BodyDef.BodyType.StaticBody;
-        bottom.position.set(new Vector2((float) WORLD_WIDTH / 2, 0));
-        var bottomBody = world.createBody(bottom);
-        var bottomBox = new PolygonShape();
-        bottomBox.setAsBox((float) WORLD_WIDTH / 2, 0.025f);
-        bottomBody.createFixture(bottomBox, 0f);
-        bottomBox.dispose();
-    }
-
     void initTerrain() {
-        var accom1 = new BodyDef();
-        accom1.type = BodyDef.BodyType.StaticBody;
-        accom1.position.set(5.5f, 42);
-        var accom1Body = world.createBody(accom1);
-        var accom1Box = new PolygonShape();
-        accom1Box.setAsBox(4.5f, 11);
-        accom1Body.createFixture(accom1Box, 0f);
-        accom1Box.dispose();
+        var json = new Json();
+        var objects = json.fromJson(PhysicsPolygon[].class, Gdx.files.internal("terrain.json"));
 
-        var accom2 = new BodyDef();
-        accom2.type = BodyDef.BodyType.StaticBody;
-        accom2.position.set(22.5f, 42);
-        var accom2Body = world.createBody(accom2);
-        var accom2Box = new PolygonShape();
-        accom2Box.setAsBox(4.5f, 11);
-        accom2Body.createFixture(accom2Box, 0f);
-        accom2Box.dispose();
+        for (var object: objects) {
+            var bodyDef = new BodyDef();
+            bodyDef.type = object.getType();
+            bodyDef.position.set(object.getPosition());
 
-        var accom3 = new BodyDef();
-        accom3.type = BodyDef.BodyType.StaticBody;
-        accom3.position.set(55.5f, 42);
-        var accom3Body = world.createBody(accom3);
-        var accom3Box = new PolygonShape();
-        accom3Box.setAsBox(4.5f, 11);
-        accom3Body.createFixture(accom3Box, 0f);
-        accom3Box.dispose();
+            var body = world.createBody(bodyDef);
+            var shape = new PolygonShape();
+            shape.set(object.getVertices());
 
-        var pond = new BodyDef();
-        pond.type = BodyDef.BodyType.StaticBody;
-        pond.position.set(12.5f, 0);
-        var pondBody = world.createBody(pond);
-        var pondPolygon = new PolygonShape();
-        pondPolygon.set(new Vector2[] {
-            new Vector2(0, 0), new Vector2(0, 1.5f), new Vector2(10, 4), new Vector2(16, 5),
-            new Vector2(24, 5), new Vector2(26, 4), new Vector2(26, 2), new Vector2(28, 0)
-        });
-        pondBody.createFixture(pondPolygon, 0f);
-        pondPolygon.dispose();
-
-        var csBuilding = new BodyDef();
-        csBuilding.type = BodyDef.BodyType.StaticBody;
-        csBuilding.position.set(7, 12);
-        var csBuildingBody = world.createBody(csBuilding);
-        var csBuildingPolygon = new PolygonShape();
-        csBuildingPolygon.set(new Vector2[] {
-            new Vector2(0, 0), new Vector2(5, -2), new Vector2(12, -2), new Vector2(17, 0),
-            new Vector2(17, 11), new Vector2(12, 13.5f), new Vector2(8, 13.5f), new Vector2(0, 9)
-        });
-        csBuildingBody.createFixture(csBuildingPolygon, 0f);
-        csBuildingPolygon.dispose();
-
-        var piazzaBuilding = new BodyDef();
-        piazzaBuilding.type = BodyDef.BodyType.StaticBody;
-        piazzaBuilding.position.set(45, 11);
-        var piazzaBuildingBody = world.createBody(piazzaBuilding);
-        var piazzaBuildingPolygon = new PolygonShape();
-        piazzaBuildingPolygon.set(new Vector2[] {
-            new Vector2(0, 0),
-            new Vector2(8, -4),
-            new Vector2(20, -4),
-            new Vector2(20, 8),
-            new Vector2(9, 8),
-            new Vector2(6, 16),
-            new Vector2(0, 16)
-        });
-        piazzaBuildingBody.createFixture(piazzaBuildingPolygon, 0f);
-        piazzaBuildingPolygon.dispose();
+            body.createFixture(shape, 0f);
+            shape.dispose();
+        }
     }
 
     Entity[] initInteractionLocations(Engine engine) {
