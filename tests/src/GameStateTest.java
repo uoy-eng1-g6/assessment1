74edf01b49ab8dd1400108dbb7d6ce0339898d12
dn813 <dn813@york.ac.uk>
2024-04-26 15:03:09 +0100

    Completed GameStateTest.java

--- tests/src/GameStateTest.java
+++ tests/src/GameStateTest.java
@@ -1,3 +1,4 @@
+import io.github.uoyeng1g6.constants.ActivityType;
 import io.github.uoyeng1g6.constants.GameConstants;
 import io.github.uoyeng1g6.models.GameState;
 import jvms.assessment2.gdxtesting.GdxTestRunner;
@@ -23,5 +24,36 @@ public class GameStateTest {
         assertEquals(GameConstants.MAX_HOURS, gameState.hoursRemaining);
         assertNotSame(new ArrayList<GameState.Day>(7), gameState.days);
         assertNotNull(gameState.interactionOverlay);
+    }
+
+    @Test
+    public void testDoActivity() {
+        GameState gameState = new GameState();
+        int timeToUse = 1;
+        int energyToUse = 10;
+        ActivityType activityType = ActivityType.MEAL;
+        String overlayText = "Text";
+        int beforeEnergy = gameState.energyRemaining;
+        int beforeHoursRemaining = gameState.hoursRemaining;
+        GameState.Day currentDay = gameState.currentDay;
+        boolean result = gameState.doActivity(timeToUse, energyToUse, activityType, overlayText);
+        assertTrue(result);
+        assertEquals(beforeEnergy - energyToUse, gameState.energyRemaining);
+        assertEquals(beforeHoursRemaining - timeToUse, gameState.hoursRemaining);
+        currentDay.activityStats.merge(activityType, 1, Integer::sum);
+        assertEquals(currentDay, gameState.currentDay);
+        gameState.energyRemaining = 5;
+        boolean result2 = gameState.doActivity(timeToUse, energyToUse, activityType, overlayText);
+        assertFalse(result2);
+    }
+
+    @Test
+    public void testGetTotalActivityCount() {
+        GameState gameState = new GameState();
+        for(ActivityType activityType : ActivityType.values()) {
+            assertEquals(0, gameState.getTotalActivityCount(activityType));
+            gameState.doActivity(1, 10, activityType, "test");
+            assertEquals(1, gameState.getTotalActivityCount(activityType));
+        }
     }
 }
