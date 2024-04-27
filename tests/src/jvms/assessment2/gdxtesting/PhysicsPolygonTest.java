6fc15ebb7f0cd8884085796f5b86ba1bf67e1512
jjzsilva9 <jjzsilva9@gmail.com>
2024-04-27 11:01:04 +0100

    adding ashley to test dependencies

--- tests/src/jvms/assessment2/gdxtesting/PhysicsPolygonTest.java
+++ tests/src/jvms/assessment2/gdxtesting/PhysicsPolygonTest.java
@@ -1,9 +1,23 @@
 package jvms.assessment2.gdxtesting;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import io.github.uoyeng1g6.models.PhysicsPolygon;
+import com.badlogic.ashley.core.Entity;
+
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class PhysicsPolygonTest {
 
     @Test
-    public void testPhysicsPolygon
+    public void testPhysicsPolygon (){
+        BodyDef body = new BodyDef();
+        body.type = BodyDef.BodyType.StaticBody;
+        Vector2[] vertices = {new Vector2(0, 0), new Vector2(1, 0)};
+        PhysicsPolygon p = new PhysicsPolygon("name", body.type, new Vector2(0, 0), vertices);
+
+        assertTrue(true);
+    }
 }
