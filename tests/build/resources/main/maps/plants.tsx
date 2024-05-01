effc8cfefdf80551564a870f8f5ea8c5b29b0656
Joe Silva <73418407+jjzsilva9@users.noreply.github.com>
2024-05-01 12:02:26 +0100

    Delete tests/build directory

--- tests/build/resources/main/maps/plants.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="plants" tilewidth="32" tileheight="32" tilecount="512" columns="16">
- <image source="plants.png" width="512" height="1024"/>
-</tileset>
